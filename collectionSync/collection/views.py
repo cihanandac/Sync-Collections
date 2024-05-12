ec04b12cb4634e6dbfd5e01df76e08a082cfebb8
Cihan Andac <cihanandac@gmail.com>
2024-05-12 23:10:50 +0200

    chores

--- collectionSync/collection/views.py
+++ collectionSync/collection/views.py
@@ -366,6 +366,8 @@ def sync_one_plone_object(ccObjectID, index_name):
 
 
 def create_update_object(museum_object):
+    plone_url = f"http://localhost:8081/Plone/nl/@@admin_fixes?op=import_collection_object&object_id={museum_object.ccObjectID}&index_name={museum_object.index_name}"
+
     response = None
 
     try:
